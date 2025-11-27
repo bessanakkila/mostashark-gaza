73ddd36f7b07b2bc96be4c446482b91c53b39250
Bessan Akkila <bessanbessan965@gmail.com>
2025-11-27 09:07:51 +0200

    تعديل الترجمة وتهيئة Vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+plugins: [react()],
+server: {
+host: true,
+port: 5173,
+strictPort: true,
+allowedHosts: ['mostashark-gaza.com', 'www.mostashark-gaza.com'],
+hmr: {
+
+
+host: "mostashark-gaza.com" ,
+port:5173
+}
+}
 })
